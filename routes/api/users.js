e62e2993f8ddf58b6976cd91e4bb7abfcd7bbcae
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-05-25 01:17:48 -0400

    Email & Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,34 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password)
+      .then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({password: 'Password incorrect'});
+      }
+    });
+  });
+});
+
+
 module.exports = router;
