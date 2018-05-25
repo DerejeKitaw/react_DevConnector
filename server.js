398f1224cd66b58e4025900e856f8e7582c9c569
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-05-24 23:43:40 -0400

    Route Files With Express Router

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB config
@@ -13,7 +17,12 @@ mongoose.connect(db)
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
-  
+
+// use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
